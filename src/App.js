e84f377dbf4bcb4a92b8e01ddf36ffd41dfdd747
takeyoshi tochihara <take.tochi0405@gmail.com>
2022-04-17 16:11:59 +0900

    App.js modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, let's enjoy
         </a>
       </header>
     </div>
